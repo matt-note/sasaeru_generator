086b09e6ba46bd609706f354e80ba8db284668c8
Shinichi Matsumoto <sugarandsalt89@gmail.com>
2019-05-21 00:40:55 +0900

    generate pdf

--- app/controllers/sasaerus_controller.rb
+++ app/controllers/sasaerus_controller.rb
@@ -10,6 +10,11 @@ class SasaerusController < ApplicationController
   # GET /sasaerus/1
   # GET /sasaerus/1.json
   def show
+    respond_to do |format|
+      format.html
+      format.pdf { send_pdf(@sasaeru) }
+      format.png { send_png(@sasaeru) }
+    end
   end
 
   # GET /sasaerus/new
@@ -70,5 +75,52 @@ class SasaerusController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def sasaeru_params
       params.require(:sasaeru).permit(:top_text, :title, :sub_title, :author, :base_color, :lead, :sub_lead_1, :sub_lead_2, :sub_lead_3, :sub_lead_4, :bottom_text)
+    end
+
+    def send_pdf(sasaeru)
+      send_data(generate_sasaeru_theme(sasaeru),
+                filename: "sasaeru.pdf",
+                type: "application/pdf",
+                disposition: "attachment")
+    end
+
+    # def send_png(sasaeru)
+    #   pdf = MiniMagick::Image.open(send_pdf(sasaeru))
+    #
+    #   MiniMagick::Tool::Convert.new do |convert|
+    #     convert << pdf.pages.first.path
+    #     convert << "png:8sasaeru.png"
+    #     png = convert
+    #   end
+    #
+    #   send_data(png,
+    #             filename: "sasaeru.png",
+    #             type: "image/png",
+    #             disposition: "attachment")
+    # end
+
+    def generate_sasaeru_theme(sasaeru)
+      file = File.join(Rails.root, "app", "assets", "reports", "sasaeru.tlf")
+      report = Thinreports::Report.new layout: file
+
+      report.start_new_page
+
+      report.page.item(:top_text).value(sasaeru.top_text)
+      report.page.item(:title).value(sasaeru.title)
+      report.page.item(:sub_title).value(sasaeru.sub_title)
+      report.page.item(:sub_title_background).style(:fill_color, sasaeru.base_color)
+
+      report.page.item(:author).value(sasaeru.author)
+
+      report.page.item(:lead).value(sasaeru.lead)
+      report.page.item(:lead).style(:color, sasaeru.base_color)
+      report.page.item(:sub_lead_1).value(sasaeru.sub_lead_1)
+      report.page.item(:sub_lead_2).value(sasaeru.sub_lead_2)
+      report.page.item(:sub_lead_3).value(sasaeru.sub_lead_3)
+      report.page.item(:sub_lead_4).value(sasaeru.sub_lead_4)
+
+      report.page.item(:bottom_text).value(sasaeru.bottom_text)
+
+      report.generate
     end
 end
